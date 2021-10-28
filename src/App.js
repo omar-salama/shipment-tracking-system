53529e93f4faf44ef5b5271b85d5e5c44b2d0c06
Omar Salama <osalama@Omars-MacBook-Pro.local>
2021-10-28 03:53:55 +0200

    added ShipmentProgress Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ShipmentProgress from "./ShipmentProgress";
 
 function App() {
+  const [shipmentDetails, setShipmentDetails] = useState("");
+
+  useEffect(() => {
+    requestDetails();
+  }, []);
+
+  const requestDetails = async () => {
+    const shipment = await axios.get(
+      "https://tracking.bosta.co/shipments/track/1094442"
+    );
+    setShipmentDetails(shipment.data);
+  };
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="container">
+        <ShipmentProgress shipment={shipmentDetails} />
+      </div>
     </div>
   );
 }
