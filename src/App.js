3683309112e38ffb9c9680385e6dff5b52cf5fda
Omar Salama <osalama@Omars-MacBook-Pro.local>
2021-10-28 03:57:47 +0200

    Revert "added ShipmentProgress Component"
    This reverts commit 53529e93f4faf44ef5b5271b85d5e5c44b2d0c06.
    
    forgot to add new (untracked) file.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import ShipmentProgress from "./ShipmentProgress";
 
 function App() {
-  const [shipmentDetails, setShipmentDetails] = useState("");
-
-  useEffect(() => {
-    requestDetails();
-  }, []);
-
-  const requestDetails = async () => {
-    const shipment = await axios.get(
-      "https://tracking.bosta.co/shipments/track/1094442"
-    );
-    setShipmentDetails(shipment.data);
-  };
   return (
     <div className="App">
-      <div className="container">
-        <ShipmentProgress shipment={shipmentDetails} />
-      </div>
+      <h1>Hello World!</h1>
     </div>
   );
 }
