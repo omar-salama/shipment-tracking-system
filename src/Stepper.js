324b5b349d48dff36e175fe463e65d77fc1c465c
Omar Salama <omarzalama@gmail.com>
2021-11-01 21:30:17 +0200

    added icons the stepper and handeled their render logic

--- src/Stepper.js
+++ src/Stepper.js
@@ -1,12 +1,26 @@
 import "./Stepper.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTicketAlt } from "@fortawesome/free-solid-svg-icons";
+import { faBoxOpen } from "@fortawesome/free-solid-svg-icons";
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 import setColorByShipmentState from "./utils/changeStyle";
 
 function Stepper({ currentShipmentState, TransitEvents }) {
+  currentShipmentState = "DELIVERED";
   const steps = [
     "تم إنشاء الشحنة",
     "تم وصول الشحنة للمخازن",
     "الشحنة خرجت للتسليم",
     "تم التسليم",
+  ];
+  const icons = [
+    <FontAwesomeIcon icon={faTicketAlt} />,
+    <FontAwesomeIcon icon={faBoxOpen} />,
+    <FontAwesomeIcon icon={faTruck} flip="horizontal" />,
+    <FontAwesomeIcon icon={faSave} />,
+    <FontAwesomeIcon icon={faCheck} />,
   ];
   const currentStepNumber = (state) => {
     switch (state) {
@@ -24,11 +38,28 @@ function Stepper({ currentShipmentState, TransitEvents }) {
         return 0;
     }
   };
+
+  // adds class names according to the shipment state
   const isComplete = (stepNumber) => {
-    return currentStepNumber(currentShipmentState) > stepNumber
-      ? "completed"
-      : "";
+    if (currentStepNumber(currentShipmentState) > stepNumber) {
+      // add special class to the last shipment state
+      if (stepNumber + 1 === currentStepNumber(currentShipmentState)) {
+        return "completed current-state";
+      }
+      return "completed";
+    }
+    return "";
   };
+  function renderIcons(index, icon) {
+    if (!isComplete(index)) return icon[index];
+    // render last shipment state icon instead of the checkmark ( √ ) icon
+    if (
+      isComplete(index) === "completed current-state" &&
+      currentStepNumber(currentShipmentState) !== 4
+    )
+      return icon[index];
+    return icon[4];
+  }
 
   const changeStepperColor = () => {
     if (currentStepNumber(currentShipmentState) === 4) return "delivered";
@@ -47,7 +78,7 @@ function Stepper({ currentShipmentState, TransitEvents }) {
         <div key={index} className={"stepper-item " + isComplete(index)}>
           <div className={"progress-bar " + changeStepperColor()}></div>
           <div className={"step-counter " + changeStepperColor()}>
-            {index + 1}
+            {renderIcons(index, icons)}
           </div>
           <div className="step-name">
             {step}
